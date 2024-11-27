ac36e3eb531cfa7c10d2ff351ae85e7f21f42ec0
ren <154307471+ren-malthalash@users.noreply.github.com>
2024-11-28 02:29:23 +0800

    Implemented Voice Recognition FAB

--- app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/MainActivity.java
+++ app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/MainActivity.java
@@ -1,35 +1,63 @@
 package com.mobdev.x22.tordillo.christiandave.vauditor;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.Manifest;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityMainBinding;
 import com.mobdev.x22.tordillo.christiandave.vauditor.ui.notifications.NotificationsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    static TextView notifCount;
+    private static TextView notifCount;
     private ActivityMainBinding binding;
     private int notificationCount = 0;
+
+    private FloatingActionButton fabVoiceRecognition;
+
+    private static SpeechRecognizer speechRecognizer;
+
+    private static Resources resources;
+
+    boolean bSpeechOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        fabVoiceRecognition = findViewById(R.id.fabVoiceRecognition);
+        bSpeechOn = false;
+
+        resources = getResources();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -42,6 +70,103 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = navHostFragment.getNavController();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        Intent speechRecognizeIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                .putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+                .putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, true)
+                .putExtra(RecognizerIntent.LANGUAGE_MODEL_FREE_FORM, true)
+                .putExtra(RecognizerIntent.EXTRA_PROMPT, true);
+        ;
+
+        fabVoiceRecognition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!bSpeechOn) {
+                    //Start listening
+                    Random rnd = new Random();
+                    fabVoiceRecognition.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.red_700)));
+
+                    speechRecognizer.startListening(speechRecognizeIntent);
+                    bSpeechOn = true;
+                } else {
+                    //Stop listening
+                    fabVoiceRecognition.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.purple_500)));
+
+                    speechRecognizer.stopListening();
+                    bSpeechOn = false;
+                }
+            }
+        });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                assert data != null;
+                Toast.makeText(getApplicationContext(), data.get(0), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        
+        if (requestCode == 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
