3d7b65c840f66b92ccf3864616726bbeb954a203
migsGUIao <miguel_guiao@dlsu.edu.ph>
2024-12-07 00:32:54 +0800

    changed account type to spinner (banking company, person) on add balance account xml

--- app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/balanceaccounts/AddBalanceAccountActivity.java
+++ app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/balanceaccounts/AddBalanceAccountActivity.java
@@ -15,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.mobdev.x22.tordillo.christiandave.vauditor.MainActivity;
 import com.mobdev.x22.tordillo.christiandave.vauditor.R;
+import com.mobdev.x22.tordillo.christiandave.vauditor.database.DatabaseManager;
 import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityAddBalanceAccountBinding;
 import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityAddTransactionBinding;
 import com.mobdev.x22.tordillo.christiandave.vauditor.model.balanceaccount.BalanceAccountModel;
@@ -34,6 +35,8 @@ public class AddBalanceAccountActivity extends AppCompatActivity {
     private EditText notes;
     private Button cancel;
     private Button save;
+    DatabaseManager dbManager;
+    private BalanceAccountType type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +48,7 @@ public class AddBalanceAccountActivity extends AppCompatActivity {
         setContentView(view);
 
         accountName = view.findViewById(R.id.et_account_name);
-        accountType = view.findViewById(R.id.et_account_type);
+        accountType = view.findViewById(R.id.spinner_account_type);
         initialBalance = view.findViewById(R.id.et_initial_balance);
         notes = view.findViewById(R.id.et_notes);
         cancel = view.findViewById(R.id.btn_cancel);
@@ -55,10 +58,18 @@ public class AddBalanceAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 BigDecimal bdAccountBalance = new BigDecimal(Long.parseLong(initialBalance.getText().toString()));
+
+                String sType = accountType.toString();
+                if (sType.equals("Banking Company")) {
+                    type = BalanceAccountType.BANKING_COMPANY;
+                } else {
+                    type = BalanceAccountType.PERSON;
+                }
+
                 // balanceAccId has a value of 1 as idk how to increment it properly
-                // BalanceAccountType temporarily set to Person
-                balanceAccountModel = new BalanceAccountModel(1, accountName.getText().toString(), BalanceAccountType.PERSON, bdAccountBalance, false);
-                balanceAccountModel.generateContentValuesWithoutId();
+                balanceAccountModel = new BalanceAccountModel(1, accountName.getText().toString(), type, bdAccountBalance, false);
+                dbManager = new DatabaseManager(getApplicationContext());
+                dbManager.insertBalanceAccount(balanceAccountModel);
             }
         });
         cancel.setOnClickListener(new View.OnClickListener() {
@@ -67,10 +78,6 @@ public class AddBalanceAccountActivity extends AppCompatActivity {
                 Intent intent = new Intent(AddBalanceAccountActivity.this, MainActivity.class);
                 startActivity(intent);
             }
-
         });
-
-
-
     }
 }
