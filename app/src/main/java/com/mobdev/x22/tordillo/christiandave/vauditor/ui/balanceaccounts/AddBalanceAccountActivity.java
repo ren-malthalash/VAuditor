49ae1093f423e9d17b569f336b7beeb6b8317d61
Christian Dave Tordillo <154307471+ren-malthalash@users.noreply.github.com>
2024-12-07 15:13:49 +0800

    Merge pull request #1
    * added transaction info is saved to database ()
    
    * added balance account info is saved to database
    
    * Added AccountType Spinner for adding Balance Accounts
    
    * dialogue box for editing balance account
    
    * changed account type to spinner (banking company, person) on add bala…
    
    * Merge remote-tracking branch 'origin/balanceaccounts' into balanceacc…

--- app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/balanceaccounts/AddBalanceAccountActivity.java
+++ app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/balanceaccounts/AddBalanceAccountActivity.java
@@ -1,6 +1,12 @@
 package com.mobdev.x22.tordillo.christiandave.vauditor.ui.balanceaccounts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +15,73 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mobdev.x22.tordillo.christiandave.vauditor.MainActivity;
 import com.mobdev.x22.tordillo.christiandave.vauditor.R;
+import com.mobdev.x22.tordillo.christiandave.vauditor.database.DatabaseManager;
+import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityAddBalanceAccountBinding;
+import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityAddTransactionBinding;
+import com.mobdev.x22.tordillo.christiandave.vauditor.model.balanceaccount.BalanceAccountModel;
+import com.mobdev.x22.tordillo.christiandave.vauditor.model.balanceaccount.BalanceAccountType;
+import com.mobdev.x22.tordillo.christiandave.vauditor.model.transactions.TransactionModel;
+import com.mobdev.x22.tordillo.christiandave.vauditor.ui.transactions.AddTransactionActivity;
+
+import java.math.BigDecimal;
 
 public class AddBalanceAccountActivity extends AppCompatActivity {
+
+    BalanceAccountModel balanceAccountModel;
+    ActivityAddBalanceAccountBinding binding;
+    private EditText accountName;
+    private Spinner accountType;
+    private EditText initialBalance;
+    private EditText notes;
+    private Button cancel;
+    private Button save;
+    DatabaseManager dbManager;
+    private BalanceAccountType type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_balance_account);
+
+        binding = ActivityAddBalanceAccountBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        accountName = view.findViewById(R.id.et_account_name);
+        accountType = view.findViewById(R.id.sp_account_type);
+        initialBalance = view.findViewById(R.id.et_initial_balance);
+        notes = view.findViewById(R.id.et_notes);
+        cancel = view.findViewById(R.id.btn_cancel);
+        save = view.findViewById(R.id.btn_save);
+
+        String[] accountTypes = new String[]{"Banking Company", "Person"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, accountTypes);
+        accountType.setAdapter(adapter);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BigDecimal bdAccountBalance = new BigDecimal(Long.parseLong(initialBalance.getText().toString()));
+                balanceAccountModel = new BalanceAccountModel(1, accountName.getText().toString(), BalanceAccountType.values()[accountType.getSelectedItemPosition()], bdAccountBalance, false);
+                balanceAccountModel.generateContentValuesWithoutId();
+                dbManager = new DatabaseManager(getApplicationContext());
+                dbManager.insertBalanceAccount(balanceAccountModel);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddBalanceAccountActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
 
     }
 }
--- app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/transactions/AddTransactionActivity.java
+++ app/src/main/java/com/mobdev/x22/tordillo/christiandave/vauditor/ui/transactions/AddTransactionActivity.java
@@ -1,5 +1,10 @@
 package com.mobdev.x22.tordillo.christiandave.vauditor.ui.transactions;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -7,6 +12,10 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -14,14 +23,56 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.mobdev.x22.tordillo.christiandave.vauditor.MainActivity;
 import com.mobdev.x22.tordillo.christiandave.vauditor.R;
+import com.mobdev.x22.tordillo.christiandave.vauditor.databinding.ActivityAddTransactionBinding;
+import com.mobdev.x22.tordillo.christiandave.vauditor.model.transactions.TransactionModel;
+
+import java.math.BigDecimal;
 
 public class AddTransactionActivity extends AppCompatActivity {
+
+    TransactionModel transactionModel;
+    ActivityAddTransactionBinding binding;
+    private EditText transactionName;
+    private EditText transactionAmount;
+    private EditText transactionNotes;
+    private Button cancel;
+    private Button save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_transaction);
+
+        binding = ActivityAddTransactionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        transactionName = view.findViewById(R.id.et_transaction_name);
+        transactionAmount = view.findViewById(R.id.et_transaction_amount);
+        transactionNotes = view.findViewById(R.id.et_transaction_notes);
+        cancel = view.findViewById(R.id.btn_cancel);
+        save = view.findViewById(R.id.btn_save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BigDecimal bdTransactionAmount = new BigDecimal(Long.parseLong(transactionAmount.getText().toString()));
+                // transactionId and transactionGroupId has a value of 1 as idk how to increment it properly
+                transactionModel = new TransactionModel(1,1,transactionName.getText().toString(), bdTransactionAmount, transactionNotes.getText().toString());
+                transactionModel.generateContentValuesWithoutId();
+            }
+
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddTransactionActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
